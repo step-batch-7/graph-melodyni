baae71597a64a147cfed302039a46111dffc5cfb
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-06-10 21:59:35 +0530

    implemented bfs and parse function

--- src/graph.js
+++ src/graph.js
@@ -1,11 +1,40 @@
-//Example 
+//Example
 // Pairs => [[from,to],[to,from]]
 // Source => from
-// To => to 
+// To => to
 // Should return true.
 
-const bfs = function(pairs,source,target){
-  
+const parse = (pairs) => {
+  const graph = {};
+  pairs.forEach(([src, target]) => {
+    if (graph[src] == undefined) {
+      graph[src] = [];
+    }
+    graph[src].push(target);
+  });
+  return graph;
 };
 
-module.exports = {bfs};
+const bfs = function (pairs, source, target) {
+  graph = parse(pairs);
+  const visited = [];
+  const queue = [];
+  queue.push(source);
+  while (queue.length != 0) {
+    const node = queue.shift();
+    visited.push(node);
+    console.log(visited);
+    const adjacent = graph[node] || [];
+    if (adjacent.includes(target)) {
+      return true;
+    }
+    adjacent.forEach((node) => {
+      if (!visited.includes(node) && !queue.includes(node)) {
+        queue.push(node);
+      }
+    });
+  }
+  return false;
+};
+
+module.exports = { bfs };
